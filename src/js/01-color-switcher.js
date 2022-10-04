732c8e9356405e5b477d03b29c217e7e515d398f
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-10-05 01:20:38 +0300

    task_1_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+const colorSwitcher = {
+  isActive: false,
+  startSwitch() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    timerId = setInterval(() => {
+      let color = getRandomHexColor();
+      bodyRef.style.backgroundColor = color;
+    }, 1000);
+  },
+
+  stopSwitch() {
+    clearInterval(timerId);
+    this.isActive = false;
+  },
+};
+
+startBtnRef.addEventListener('click', () => {
+  colorSwitcher.startSwitch();
+});
+
+stopBtnRef.addEventListener('click', () => {
+  colorSwitcher.stopSwitch();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
