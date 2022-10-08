8a99735d31312c8f9b79d861fe1fd23a8592a018
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-10-08 05:20:04 +0300

    task_3_done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -53,27 +53,18 @@ function onClick() {
 }
 
 function calculateTimerValue() {
-  const days = Math.floor(leftTime / 1000 / 60 / 60 / 24)
+  daysValRef.textContent = Math.floor(leftTime / 1000 / 60 / 60 / 24)
     .toString()
     .padStart(2, '0');
-  const hours = (Math.floor(leftTime / 1000 / 60 / 60) % 24)
+  hoursValRef.textContent = (Math.floor(leftTime / 1000 / 60 / 60) % 24)
     .toString()
     .padStart(2, '0');
-  const minutes = (Math.floor(leftTime / 1000 / 60) % 60)
+  minutesValRef.textContent = (Math.floor(leftTime / 1000 / 60) % 60)
     .toString()
     .padStart(2, '0');
-  const seconds = (Math.floor(leftTime / 1000) % 60)
+  secondsValRef.textContent = (Math.floor(leftTime / 1000) % 60)
     .toString()
     .padStart(2, '0');
-
-  showTimerValue({ days, hours, minutes, seconds });
-}
-
-function showTimerValue({ days, hours, minutes, seconds }) {
-  daysValRef.textContent = days;
-  hoursValRef.textContent = hours;
-  minutesValRef.textContent = minutes;
-  secondsValRef.textContent = seconds;
 }
 
 function stopTimerIf() {
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,77 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const changeSettings = Notify.init({
+  position: 'center-top',
+});
+
+const formRef = document.querySelector('.form');
+const submitBtnRef = document.querySelector('button');
+
+const inputsValue = {};
+
+formRef.addEventListener('input', onInput);
+submitBtnRef.addEventListener('click', onSubmit);
+
+function onInput(e) {
+  if (Number(e.target.value) < 0) {
+    Notify.failure('The value cannot be negative');
+    e.target.value = '';
+  }
+  getValue(e);
+}
+
+function getValue(e) {
+  let userTarget = e.target.name;
+  switch (userTarget) {
+    case 'delay':
+      inputsValue.delay = Number(e.target.value);
+      break;
+    case 'step':
+      inputsValue.step = Number(e.target.value);
+      break;
+    case 'amount':
+      inputsValue.amount = Number(e.target.value);
+      break;
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  inputsValue.positon = 1;
+  let submitDelay = inputsValue.delay;
+
+  for (let i = 0; i < inputsValue.amount; i += 1) {
+    createPromise(inputsValue.positon, submitDelay);
+    inputsValue.positon += 1;
+    submitDelay += inputsValue.step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const result = {
+      position,
+      delay,
+    };
+    if (shouldResolve) {
+      resolve(result);
+    } else {
+      reject(result);
+    }
+  });
+  promise
+    .then(({ position, delay }) => {
+      setTimeout(
+        () => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`),
+        delay
+      );
+    })
+    .catch(({ position, delay }) => {
+      setTimeout(
+        () => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`),
+        delay
+      );
+    });
 }
